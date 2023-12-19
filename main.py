d0362a97e5aad2b32109b6b84a599ae22a99ebf8
daviidarr <nononymous@gmail.com>
2023-12-19 15:57:15 +0100

    validate pools, plot rewards, truncate...

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import html
 import shutil
 import sys
 import json
@@ -22,18 +23,12 @@ import streamlit as st
 import plotly.express as px
 
 from scrappers.defillama_history.coingecko import myCoinGeckoAPI
-from utils.io_utils import plot_perf, human_format, login_prompt, welcome
+from utils.io_utils import plot_perf, human_format, check_whitelist
 
 st.title('Yield optimizer backtest \n by sety-project.eth', help='0xFaf2A8b5fa78cA2786cEf5F7e19f6942EC7cB531')
 
-'''
-authentification
-'''
-st.session_state.authentification = st.session_state.get("authentification", "unverified")
-if st.session_state.authentification != "verified":
-    login_prompt()
-    st.stop()
-welcome()
+st.session_state.authentification = "unverified"
+#st.session_state.user_tg_handle = ''
 
 with st.spinner('fetching meta data'):
     coingecko = myCoinGeckoAPI()
@@ -42,56 +37,60 @@ with st.spinner('fetching meta data'):
 initialize, backtest, analytics = st.tabs(["pool selection", "backtest", "analytics"])
 # load whitelisted protocols from yaml
 with initialize:
-    with st.form("initialize"):
+    with st.form("pool_universe"):
         # instantiate defillama API
         use_oracle = st.selectbox("use_oracle", options=[False, True], help="this is for depeg snipping. Please use none for now",
                      disabled=True)
         reference_asset = st.selectbox("reference_asset", options=['usd', 'eth', 'btc'], help="What asset you are investing")
 
         top_chains = coingecko.address_map.count().sort_values(ascending=False)[2:23].index
-        chains = st.multiselect("chains", options=top_chains, default=['Arbitrum'], help="select chains to include in universe")
+        chains = st.multiselect("chains", options=top_chains, default=['Arbitrum', 'Optimism', 'Polygon', 'Avalanche', 'Base'], help="select chains to include in universe")
 
-        tvl_threshold = np.power(10, st.slider("protocol tvl threshold (log10)", min_value=0.5, value=7., max_value=10.,
+        tvl_threshold = np.power(10, st.slider("protocol tvl threshold (log10)", min_value=0.5, value=6., max_value=10.,
                                                step=0.1, help="log10 of the minimum tvl to include in universe"))
         st.write(f'protocol tvl threshold = {human_format(tvl_threshold)}')
         tvlUsd_floor = np.power(10,
-                                st.slider("pool tvl threshold (log10)", min_value=0.5, value=6., max_value=10., step=0.1,
+                                st.slider("pool tvl threshold (log10)", min_value=0.5, value=5., max_value=10., step=0.1,
                                           help="log10 of minimum tvl to include in universe"))
         st.write(f'pool tvl threshold = {human_format(tvlUsd_floor)}')
         if submitted := st.form_submit_button("Find pools"):
-            defillama = FilteredDefiLlama(reference_asset=reference_asset,
+            st.session_state.defillama = FilteredDefiLlama(reference_asset=reference_asset,
                                                            chains=chains,
                                                            oracle=coingecko,
                                                            protocol_tvl_threshold=tvl_threshold,
                                                            pool_tvl_threshold=tvlUsd_floor,
                                                            use_oracle=use_oracle)
-            st.session_state.defillama = defillama
-            st.write(f'found {len(defillama.pools)} pools among {len(defillama.protocols)} protocols')
-            meta_df = defillama.pools.copy()
+            st.write(f'found {len(st.session_state.defillama.pools)} pools among {len(st.session_state.defillama.protocols)} protocols')
+    if 'defillama' in st.session_state:
+        with st.form("pool_selection"):
+            meta_df = st.session_state.defillama.pools.copy()
             meta_df['underlyingTokens'] = meta_df.apply(lambda meta: [coingecko.address_to_symbol(address, meta['chain'])
-                                                                      for address in meta['underlyingTokens']] if meta['underlyingTokens'] else [],
+                                                                      for address in meta['underlyingTokens']] if meta[
+                'underlyingTokens'] else [],
                                                         axis=1)
-            meta_df['rewardTokens'] = meta_df.apply(lambda meta: [coingecko.address_to_symbol(address.lower(), meta['chain'])
-                                                                      for address in meta['rewardTokens']] if meta['rewardTokens'] else [],
-                                                        axis=1)
-            st.dataframe(meta_df[['chain', 'project', 'underlyingTokens', 'tvlUsd', 'apy', 'apyBase', 'apyReward', 'rewardTokens', 'predictedClass', 'binnedConfidence']],
+            meta_df['rewardTokens'] = meta_df.apply(
+                lambda meta: [coingecko.address_to_symbol(address.lower(), meta['chain'])
+                              for address in meta['rewardTokens']] if meta['rewardTokens'] else [],
+                axis=1)
+            meta_df['selected'] = True
+            edited_meta = st.data_editor(meta_df[['selected', 'chain', 'project', 'underlyingTokens', 'tvlUsd', 'apy', 'apyReward', 'rewardTokens', 'predictedClass', 'binnedConfidence']],
                          use_container_width=True, hide_index=True)
-            st.session_state.initialized = True
+            if submitted := st.form_submit_button("Validate pools"):
+                st.session_state.defillama.pools = st.session_state.defillama.pools[edited_meta['selected']]
+                st.session_state.initialized = True
 
     if 'initialized' in st.session_state:
-        with st.form("data"):
-            if submitted := st.form_submit_button("Fetch data"):
-                with st.status('Fetching data...', expanded=True) as status:
-                    if os.path.isdir(os.path.join(os.sep, os.getcwd(), 'data', 'latest')):
-                        shutil.rmtree(os.path.join(os.sep, os.getcwd(), 'data', 'latest'))
-                    try:
-                        all_history = st.session_state.defillama.all_apy_history(dirname=os.path.join(os.sep, os.getcwd(), 'data', 'latest'),
-                                                                                 status=status)
-                        status.update(label=f'Fetched {len(all_history)} pools', state='complete', expanded=True)
-                    except Exception as e:
-                        status.update(label=f'Failed to fetch data: {e}', state='error', expanded=True)
-                        raise e
-                    st.session_state.data_done = True
+        with st.status('Fetching data...', expanded=True) as status:
+            if os.path.isdir(os.path.join(os.sep, os.getcwd(), 'data', 'latest')):
+                shutil.rmtree(os.path.join(os.sep, os.getcwd(), 'data', 'latest'))
+            try:
+                all_history = st.session_state.defillama.all_apy_history(dirname=os.path.join(os.sep, os.getcwd(), 'data', 'latest'),
+                                                                         status=status)
+                status.update(label=f'Fetched {len(all_history)} pools', state='complete', expanded=True)
+            except Exception as e:
+                status.update(label=f'Failed to fetch data: {e}', state='error', expanded=True)
+                raise e
+            st.session_state.data_done = True
 
 with backtest:
     if 'data_done' in st.session_state:
@@ -125,14 +124,19 @@ with backtest:
             st.write('initial_wealth = {}'.format(human_format(override_grid['strategy.initial_wealth'][0])))
             end_date = st.date_input("backtest end", value=date.today())
             start_date = st.date_input("backtest start", value=end_date - timedelta(days=90))
+            st.session_state.user_tg_handle = st.text_input("Enter tg handle to backtest:")
 
             if submitted := st.form_submit_button("Run backtest"):
                 parameters['backtest']['end_date'] = end_date.isoformat()
                 parameters['backtest']['start_date'] = start_date.isoformat()
-                progress_bar = st.progress(value=0.0, text='Running backtest...')
-                result = VaultBacktestEngine.run_grid(override_grid, parameters, progress_bar)
-                progress_bar.progress(value=1., text=f'Completed backtest')
-                st.session_state.backtest_done = True
+
+                if st.session_state.authentification != "verified":
+                    check_whitelist()
+                if st.session_state.authentification =='verified':
+                    progress_bar = st.progress(value=0.0, text='Running backtest...')
+                    result = VaultBacktestEngine.run_grid(override_grid, parameters, progress_bar)
+                    progress_bar.progress(value=1., text=f'Completed backtest')
+                    st.session_state.backtest_done = True
 
 with analytics:
     if 'backtest_done' in st.session_state:
@@ -144,21 +148,38 @@ with analytics:
         width = 1500
 
         #plot_perf(backtest, override_grid['strategy.base_buffer'][0], height=height, width=width)
-        apy = (backtest['full_apy'] * backtest['weights'].divide(backtest['weights']['total'], axis=0)).drop(
+        apy = (backtest['apy'] * backtest['weights'].divide(backtest['weights']['total'], axis=0)).drop(
             columns='total').reset_index().melt(id_vars='index', var_name='pool', value_name='apy').dropna()
-        apy['apy'] = apy['apy'] * 100
+        apyReward = (backtest['apyReward'] * backtest['weights'].divide(backtest['weights']['total'], axis=0)).drop(
+            columns='total').reset_index().melt(id_vars='index', var_name='pool', value_name='apy').dropna()
+        apyReward['pool'] = apyReward['pool'].apply(lambda x: f'reward_{x}')
+        apy['apy'] = apy['apy'] - apyReward['apy']
+        apy = pd.concat([apy, apyReward], axis=0)
+        apy['apy'] = apy['apy']*100
 
-        avg_apy = (backtest['full_apy'] * backtest['weights'].divide(backtest['weights']['total'], axis=0)).mean()*100
-        avg_apy = avg_apy.drop(index='total').rename('apy_pc').reset_index().dropna()
-        st.plotly_chart(px.pie(avg_apy, names='index', values='apy_pc', title='apy * weights (total apy was {}_'.format(int(avg_apy['apy_pc'].sum()), height=height, width=width)))
-        st.plotly_chart(px.bar(apy, x='index', y='apy', color='pool', title='apy', height=height, width=width))
+        avg_apy = apy.groupby('pool').mean()
+        truncated_avg_apy = avg_apy[avg_apy['apy'] > 0.1].reset_index()
 
-        long_weights = backtest['weights'].drop(columns='total').reset_index().melt(id_vars='index', var_name='pool',
+        # show avg over backtest
+        st.write('total apy = {}%'.format(int(avg_apy['apy'].sum())))
+        st.plotly_chart(px.pie(truncated_avg_apy, names='pool', values='apy', title='apy * weights (%)', height=height/2, width=width/2))
+
+        # show all apy drivers over time
+        truncated_apy = apy[apy['pool'].isin(truncated_avg_apy['pool'])]
+        st.plotly_chart(px.bar(truncated_apy, x='index', y='apy', color='pool', title='apy (%)', height=height, width=width))
+
+        # show all weights over time
+        weights = backtest['weights'].drop(columns='total').reset_index().melt(id_vars='index', var_name='pool',
                                                                                     value_name='weights').dropna()
-        st.plotly_chart(px.bar(long_weights, x='index', y='weights', color='pool', title='Allocations', height=height, width=width))
+        truncated_weights = weights[weights['pool'].isin(truncated_avg_apy['pool'])]
+        st.plotly_chart(px.bar(truncated_weights, x='index', y='weights', color='pool', title='Allocations ($)', height=height, width=width))
 
-        dilutor = 100*(1 - backtest['dilutor'].drop(columns='total').dropna())
-        st.plotly_chart(px.line(dilutor, title='pct of tvl', height=height, width=width))
+        # show all dilutors over time
+        dilutor = backtest['dilutor'].drop(columns='total').reset_index().melt(id_vars='index', var_name='pool',
+                                                                     value_name='dilutor').dropna()
+        dilutor['dilutor'] = 100 * (1 - dilutor['dilutor'])
+        truncated_dilutor = dilutor[dilutor['pool'].isin(truncated_avg_apy['pool'])]
+        st.plotly_chart(px.line(truncated_dilutor, x='index', y='dilutor', color='pool', title='alloc / tvl (%)', height=height,width=width))
 
 if __name__ == "2__main__":
     args, kwargs = extract_args_kwargs(sys.argv)
