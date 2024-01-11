2ddbbccc80601960994ab4c3b3a027d110768241
daviidarr <nononymous@gmail.com>
2024-01-11 23:13:11 +0100

    allow shared connection, fix update time

--- utils/postgres.py
+++ utils/postgres.py
@@ -21,27 +21,29 @@ class SqlApi:
                            'il': Float,
                            'tvl': Float}
         self.engine: Engine = create_engine(database)
-        self.connection: Connection = self.engine.connect()
         self.tables: list = self.list_tables()
 
     #@st.cache_data
-    def read_sql_query(_self, query: str, **kwargs):
+    def read_sql_query(_self, query: str, connection: Connection = None, **kwargs):
         '''st.cache_data doesn't support async functions so we have this wrapper
         the leading underscore is also to accommodate st.cache_data'''
-        return pd.read_sql_query(sql_text(query), con=_self.connection, **kwargs)
+        if connection:
+            return pd.read_sql_query(sql_text(query), con=connection, **kwargs)
+        with _self.engine.connect() as con:
+            return pd.read_sql_query(sql_text(query), con=con, **kwargs)
 
-    async def read_one(self, instrument: str):
+    async def read_one(self, instrument: str, connection: Connection = None):
         query = f'''SELECT * FROM \"{instrument}\" ORDER BY date ASC;'''
-        result = await async_wrap(self.read_sql_query)(query, index_col='date')
+        result = await async_wrap(self.read_sql_query)(query, index_col='date', connection=connection)
         result.index = [pd.to_datetime(x, unit='ns', utc=True) for x in result.index]
         return result[~result.index.duplicated()]
 
-    async def write(self, data: pd.DataFrame, pool: str) -> str:
+    async def write(self, data: pd.DataFrame, pool: str, connection: Connection = None) -> str:
         '''write to db
          if table exists, only write new data
          add 'updated' column'''
-        if pool in self.list_tables:
-            existing_dates = self.read_sql_query(f"""SELECT date FROM \"{pool}\"""")
+        if pool in self.list_tables():
+            existing_dates = self.read_sql_query(f"""SELECT date FROM \"{pool}\"""", connection=connection)
             new_data = data[~data['date'].isin(existing_dates['date'].apply(lambda t: pd.to_datetime(t, infer_datetime_format=True, utc=True, unit='ns', errors='coerce')))]
             result = f"Added {new_data.shape[0]} rows to {pool}"
         else:
@@ -50,26 +52,26 @@ class SqlApi:
 
         if not new_data.empty:
             new_data['updated'] = datetime.now(timezone.utc)
-            await async_wrap(new_data.to_sql)(name=pool, con=self.engine, if_exists='append', index=False, dtype=self.schema)
+            await async_wrap(new_data.to_sql)(name=pool, con=connection or self.engine, if_exists='append', index=False, dtype=self.schema)
+        else:
+            result = f"No new data for {pool}"
         return result
 
-    def read_metadata(self):
+    def read_metadata(self, connection: Connection = None):
         query = '''SELECT * FROM metadata;'''
-        return self.read_sql_query(query, index_col='name')
+        return self.read_sql_query(query, index_col='name', connection=connection)
 
-    def write_metadata(self, data: pd.DataFrame):
-        data['updated'] = datetime.now(timezone.utc)
-        data.to_sql(name='metadata', con=self.engine, if_exists='replace', index=False,
+    def write_metadata(self, data: pd.DataFrame, connection: Connection = None):
+        data.to_sql(name='metadata', con=connection or self.engine, if_exists='append', index=False, # TODO: replace with upsert
                                           dtype=self.schema)
         return "updated metadata"
 
-    async def last_updated(self, metadata: dict = None):
-        if 'metadata' in self.tables:
-            prev_metadata = self.read_metadata()
+    async def last_updated(self, metadata: dict = None, prev_metadata: pd.DataFrame = None, connection: Connection = None) -> datetime:
+        if prev_metadata and metadata['name'] in prev_metadata.index:
             return prev_metadata.loc[metadata["name"], 'updated']
-        elif metadata['name'] in self.tables:
+        if metadata['name'] in self.tables:
             query = f'''SELECT MAX(updated) FROM \"{metadata['name']}\";'''
-            result = await async_wrap(pd.read_sql_query)(sql_text(query), con=self.connection)
+            result = await async_wrap(pd.read_sql_query)(sql_text(query), con=connection or self.engine)
             return result.squeeze()
         else:
             return None
@@ -86,7 +88,6 @@ class SqlApi:
         meta = MetaData()
         meta.reflect(bind=self.engine)
         return list(meta.tables)
-
 
 if __name__ == '__main__':
     sql_obj = SqlApi(st.secrets[sys.argv[2]])
